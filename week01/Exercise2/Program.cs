743b435e4cd991c390dac103c98d2e64d808fd2d
Felix Wilson Gbedemah <afrogbede09@gmail.com>
2025-05-05 13:47:27 +0000

    Updated the exercise 2 with a conditional

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Input your score: ");
+        string input = Console.ReadLine();
+        int score = int.Parse(input);
+
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80 && score < 90)
+        {
+            letter = "B";
+        }
+        else if (score >= 70 score < 80)
+        {
+            letter = "C";
+        }
+        else if (score >= 60 && score < 70)
+        {
+            letter = "D";
+        }
+        else if (score >= 0 && score < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Invalid score. Please enter a number between 0 and 100.");
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the course. Better luck next time!");
+        }
     }
-}
+}   
