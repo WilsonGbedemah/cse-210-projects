d62ecebda0f6b12e854e87bd0f2cff3ac1690bbf
Felix Wilson Gbedemah <afrogbede09@gmail.com>
2025-05-20 04:05:46 +0000

    Fractions program greated with the fraction class and using encapsulation with getters and setter

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,62 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        if (_bottom == 0)
+        {
+            throw new DivideByZeroException("Denominator cannot be zero.");
+        }
+        return (double)_top / _bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _bottom = bottom;
+    }
+
+}
