80b4f88c6b4e6ff9ab1a650157700d6ed91c440d
Felix Wilson Gbedemah <afrogbede09@gmail.com>
2025-06-02 05:36:32 +0000

    Corrections and additions to main program file

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,16 +5,16 @@ class Program
     static void Main(string[] args)
     {
         // Assignment
-        Assignment assignment = new Assignment("Felix Wilson Gbedemah", "Number Bases");
-        Console.WriteLine(assignment.GetSummary() + '\n');
+        Assignment assignment = new Assignment("Felix Wilson Gbedemah", "Multiplication");
+        Console.WriteLine(assignment.GetSummary() + '\n');s
 
         // MathAssignment
-        MathAssignment mathAssignment = new MathAssignment("King Pharaoh", "Angles", "Section 2.3", "Problems 1-10");
+        MathAssignment mathAssignment = new MathAssignment("King Pharaoh", "Angles", "Section 2.3", "Problems 4-10");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList() + '\n');
 
         // WritingAssignment
-        WritingAssignment writingAssignment = new WritingAssignment("Belinda Bankam", "Formal Letters", "Application letter for a job as a Software Engineer");
+        WritingAssignment writingAssignment = new WritingAssignment("Belinda Bankam", "European History", "The Causes of World War II by Mary Waters");
         Console.WriteLine(writingAssignment.GetSummary());
         Console.WriteLine(writingAssignment.GetWritingInformation() + '\n');
     }
