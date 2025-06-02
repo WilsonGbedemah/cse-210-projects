fc59328c0ddeab4d8a889b78a68fb570e8c2640b
Felix Wilson Gbedemah <afrogbede09@gmail.com>
2025-06-02 05:33:39 +0000

    Worked on the Homework assignment project of Inherirtance classes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Assignment
+        Assignment assignment = new Assignment("Felix Wilson Gbedemah", "Number Bases");
+        Console.WriteLine(assignment.GetSummary() + '\n');
+
+        // MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("King Pharaoh", "Angles", "Section 2.3", "Problems 1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList() + '\n');
+
+        // WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Belinda Bankam", "Formal Letters", "Application letter for a job as a Software Engineer");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation() + '\n');
     }
 }
